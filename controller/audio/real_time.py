3f136550b552a6a8dfea0eb717121c305a766324
mstfckrc <mstfckrc277@gmail.com>
2024-12-30 18:22:37 +0300

    Güncel

--- controller/audio/real_time.py
+++ controller/audio/real_time.py
@@ -1,6 +1,6 @@
 from globals import audio_data
 
-## noinspection PyArgumentEqualDefault
+# NOSONAR: Kullanılmayan parametreler callback uyumluluğu için gereklidir.
 def real_time_audio_histogram(indata , frames, time, status):
     """Ses verisinden histogram oluşturur ve günceller."""
     audio_data.extend(indata[:, 0])
